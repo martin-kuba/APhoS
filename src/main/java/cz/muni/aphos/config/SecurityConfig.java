17887972ecb81eab2e210412e10fa06d7d5bd7f1
Martin Kuba <makub@ics.muni.cz>
2024-10-21 16:14:46 +0200

    updated Spring Boot to 3.3.4

--- src/main/java/cz/muni/aphos/config/SecurityConfig.java
+++ src/main/java/cz/muni/aphos/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -25,7 +26,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authz) -> authz
                         .requestMatchers(
                                 "/search", "/", "/about", "/reference/**", "/reference/js/**",
